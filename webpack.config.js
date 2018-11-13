8f616e4ed1c118e937144ae17c41cb4cae598293
Bulat Mendagaziev <mendagazievb@gmail.com>
2018-11-14 03:17:50 +0400

    added config webpack

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+
+    entry: {
+      main: './scripts/main'
+    },
+
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+
+    resolve: {
+      extensions: ['.js', '.css']
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProd,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => isProd
+                  ? [require('postcss-preset-env')(), require('cssnano')()]
+                  : [require('postcss-preset-env')()]
+              }
+            }
+          ]
+        }
+      ]
+    },
+
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      hot: !isProd
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack template',
+        template: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  };
+};
